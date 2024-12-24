4f7bd4c5c812b8eae363108676325c26893f0909
Yaminikanta-code <yaminikantaprof1@gmail.com>
2024-12-25 00:48:02 +0530

    setup store and add authSlice

--- src/store/authSlice.js
+++ src/store/authSlice.js
@@ -9,10 +9,19 @@ const authSlice = createSlice({
   name: "auth",
   initialState,
   reducers: {
+    /**
+     * Sets the authentication status to true and stores the user data in the state.
+     * @param {Object} action The action object containing the user data.
+     * @property {Object} action.payload The user data object.
+     */
     login: (state, action) => {
       state.status = true;
       state.userData = action.payload;
     },
+
+    /**
+     * Sets the authentication status to false and clears the user data from the state.
+     */
     logout: (state) => {
       state.status = false;
       state.userData = null;
