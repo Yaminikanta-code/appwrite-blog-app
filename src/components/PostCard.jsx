11bdac9162f839578bc0e106430d090a510ac717
Yaminikanta-code <yaminikantaprof1@gmail.com>
2025-01-15 19:45:44 +0530

    debugging

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -5,18 +5,18 @@ import { Link } from "react-router-dom";
 function PostCard({ $id, title, featuredImage, content }) {
   return (
     <Link to={`/post/${$id}`}>
-      <div className="w-full bg-white shadow-lg rounded-lg">
+      <div className="w-full bg-white shadow-lg h-[50vh] rounded-lg">
         <div className="relative w-full h-auto">
           <img
             src={fileService.getFilePreview(featuredImage)}
             alt="Blog Cover"
-            className="w-full h-full object-cover rounded-t-lg"
+            className="w-full h-[28vh] object-cover rounded-t-lg"
           />
         </div>
         <div className="p-6">
           <h1 className="text-2xl font-title text-neutral-950 mb-4">{title}</h1>
           <p className="text-neutral-800">
-            {content.length > 100 ? content.substring(0, 100) + "..." : content}
+            {content.length > 60 ? content.substring(0, 60) + "..." : content}
           </p>
         </div>
       </div>
